aa0fe5fc75309ba9c5f7024a74797dc583e71ade
Arunava <learningdroidarunava@gmail.com>
2018-12-29 12:47:14 +0530

    Added code to get the content image and style image as arguments and load the images

--- style_transfer.py
+++ style_transfer.py
@@ -7,6 +7,19 @@ import argparse
 from PIL import Image
 from torchvision import transforms, models
 
+# Get the argument parset and take command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--content-image',
+    type=str,
+    required=True,
+    help='The path to the content image')
+
+parser.add_argument('-s', '--style-image',
+    type=str,
+    required=True,
+    help='The path to the style image')
+
+FLAGS, unparsed = parser.parse_known_args()
 
 # Get the pretrained features of model
 vgg = models.vgg19(pretrained=True).features
@@ -20,4 +33,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 vgg.to(device)
 
 # Load the content image
-content = load_image('')
+content = load_image(FLAGS.content_image).to(device)
+
+# Load the style image
+style = load_image(FLAGS.style_image).to(device)
